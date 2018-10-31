30b2d4f98b49a7ddf25cca6564ad955d6ea2b43e
FlyingSligGuard <FlyingSligGuard@users.noreply.github.com>
2018-10-31 02:58:18 +0000

    Updated build.sh to include new https index url
    http://pypi.python.org/simple doesn't work anymore and asks to use https instead, so the build script needed to be updated

--- com.djailla.online-video-downloader/build.sh
+++ com.djailla.online-video-downloader/build.sh
@@ -5,9 +5,9 @@ install -m 755 /home/source/rc.local /etc
 apt-get update
 apt-get install -y -q locales
 
-pip install humanize
-pip install bottle==0.12.9
-pip install youtube-dl==2016.12.01
+pip install humanize -i https://pypi.python.org/simple
+pip install bottle==0.12.9 -i https://pypi.python.org/simple
+pip install youtube-dl==2016.12.01 -i https://pypi.python.org/simple
 ln -s /usr/local/bin/youtube-dl /usr/bin/
 
 mkdir -m 755 -p /opt/youtube
